95614c0f437e199e624f05bf7e22d900acb5ff61
psubing <psubing@gmail.com>
2018-08-27 12:24:52 -0700

    test

--- test/e2e.spec.js
+++ test/e2e.spec.js
@@ -29,7 +29,7 @@ describe('express', () => {
       .then((text) => {
         console.log(text)
         expect(text).to.include('Origin Movie Finder');
-        document()
+        done()
       })
   );
 
