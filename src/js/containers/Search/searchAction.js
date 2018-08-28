889c9f4d2e8368a0b12e64da9b881e82a8de9964
psubing <psubing@gmail.com>
2018-08-28 11:16:31 -0700

    test4

--- src/js/containers/Search/searchAction.js
+++ src/js/containers/Search/searchAction.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getMovies = (searchInput) => ({
   type: 'GET_MOVIES',
-  payload: axios.get('http://www.omdbapi.com/?s=' + searchInput + '&type=movie&apikey=' + '5fa5a0bd').catch(err => { console.log(err); alert('The movie does not exist!') })
+  payload: axios.get('https://www.omdbapi.com/?s=' + searchInput + '&type=movie&apikey=' + '5fa5a0bd').catch(err => { console.log(err); alert('The movie does not exist!') })
 });
 
 export function inputSearch(searchInput) {
@@ -14,5 +14,5 @@ export function inputSearch(searchInput) {
 
 export const getInfo = (omdbID) => ({
   type: 'GET_TITLE',
-  payload: axios.get('http://www.omdbapi.com/?i=' + omdbID + '&apikey=' + '5fa5a0bd').catch(err => { console.log(err); alert('The movie does not exist!') })
+  payload: axios.get('https://www.omdbapi.com/?i=' + omdbID + '&apikey=' + '5fa5a0bd').catch(err => { console.log(err); alert('The movie does not exist!') })
 });
